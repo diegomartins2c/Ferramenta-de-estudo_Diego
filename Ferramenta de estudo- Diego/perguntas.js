ff6a3648d1017406dc5efda53df91fbb1668f0de
diegomartins2c <00001117540790sp@al.educacao.sp.gov.br>
2024-11-18 21:51:11 -0300

    Add files via upload

---
+++ Ferramenta de estudo- Diego/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'geografia',
+    'qual a capital da Italia?',
+    'a capial da Italia é a Roma'
+)
+
+criaCartao(
+    'Língua inglesa'
+    'Como se diz tchau em inglês?',
+      'bye'
+)
+criaCartao(
+    'história'
+    'quem descobriu o Brasil?',
+    'Pedro Álvarez cabral'
+)
+
+criaCartao(
+    'matemática',
+    'quantos é 2 + 2?',
+    '2+2 é igual á 4'
+)
